1971b90eda2028c55301ef1d1e0d1d5652cfefce
jackmb <40642637+jackmb@users.noreply.github.com>
2020-04-05 15:39:44 -0400

    Removed unnecessary imports from Trade.java.

--- src/Trade.java
+++ src/Trade.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Trade {
     int timestamp;
