863717e120a06d0afdea22aea7cd8fbe6db2656d
jackmb <40642637+jackmb@users.noreply.github.com>
2020-04-11 17:20:15 -0400

    Added Ashish's randomNetworth() function to Population.java.

--- src/Population.java
+++ src/Population.java
@@ -3,30 +3,30 @@ import java.util.Random;
 public class Population {
 
     public static class Client {
-        private int id;
+        private int id, trades;
         private boolean pdt;
-        //private double netWorth; //Scrapped idea - maybe implement later
+        private double netWorth;
 
         private Client(int _id) {
             id = _id;
-            if(id < 3574) {
-                pdt = false;
-            } else {
-                pdt = true;
-            }
-            //netWorth = getNetWorth();
+            netWorth = randomNetworth(id);
+            pdt = (netWorth <= 25000.00) ? true : false;
         }
 
-        /*public double getNetWorth() {
+        public static double randomNetworth(int id){
             Random r = new Random();
             if (id >= 0 && id < 3574){ // top 57% , unlimited trades
-                return (double)((int)(((r.nextGaussian() * 5000.0) + 30000.0) * 100)) / 100;
+                double x = 1;
+                while (x < 25000.0) x = Math.abs(r.nextGaussian() * 37500.0 + 4166.33);
+                return x;
             } else {
-                return (double) ((int) (((r.nextGaussian() * 5000.0) + 19999.0) * 100)) / 100;
+                double x = 25000.0;
+                while (x >= 25000.0) x = Math.abs(r.nextGaussian() * 12500.0 + 4166.33);
+                return x;
             }
-        }*/
+        }
         public String toString(){
-            return "id: " + id + "\tpdt: $"+ pdt;
+            return "id: " + id + "\n\tnetWorth: $"+ netWorth + "\n\tpdt: " + pdt;
         }
 
     }
