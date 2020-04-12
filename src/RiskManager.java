3ded7f870f193e24d349999ef454ac4f1cf3ad62
jackmb <40642637+jackmb@users.noreply.github.com>
2020-04-11 20:08:07 -0400

    Started Simulator and RiskManager

--- src/RiskManager.java
+++ src/RiskManager.java
@@ -1,5 +1,38 @@
-public class RiskManager {
-    private static int threshhold = 100;
-    private int riskFactor = 0;
+import java.util.Random;
 
+public class RiskManager {
+    private boolean tooRisky;
+    public Population.Client client;
+    public Trade trade;
+    private Random r = new Random();
+
+    public RiskManager() {}
+
+    public void randomRisk(Trade t) {
+        boolean risky = (r.nextInt(1000) == 0);
+        if(risky) {
+            t.tooRisky = true;
+        }
+    }
+
+    private void cantAffordBuyRisk(Trade t) {
+        double buyPrice = (t.price) * (double)(t.shares);
+        if(t.client.getNetworth() < buyPrice) {
+            t.tooRisky = true;
+        }
+    }
+
+    public String findBadTradeReason(Trade t) {
+        double buyPrice = (t.price) * (double)(t.shares);
+        if(t.client.getNetworth() < buyPrice) {
+            return "Client cannot afford order.";
+        }
+        else {
+            return "Price-Time Priority not met.";
+        }
+    }
+
+    private void tooManySharesCheck(Trade t) {
+
+    }
 }
