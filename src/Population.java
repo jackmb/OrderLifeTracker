459d7618b795028714288a6127d5cfb61a916b49
Ash <singh.ashish1998@gmail.com>
2020-04-15 20:30:19 -0400

    Add files via upload

--- src/Population.java
+++ src/Population.java
@@ -9,19 +9,24 @@ public class Population {
         public boolean pdt;
         public double netWorth;
         public ArrayList<Trade> tradesmade;
-
+        public double initNW = 0;
         private Client(int _id) {
             id = _id;
             netWorth = randomNetworth(id);
             pdt = netWorth <= 25000.00;
             tradesmade = new ArrayList<>();
+            initNW = netWorth;
         }
 
         public static double randomNetworth(int id){
             Random r = new Random();
-            if (id >= 0 && id < 3574){ // top 57% , unlimited trades
+            if (id >= 0 && id < 62) {
                 double x = 1;
-                while (x < 25000.0) x = Math.abs(r.nextGaussian() * 37500.0 + 4166.33);
+                while (x < 5000000.0) x = Math.abs(r.nextGaussian() * 10000000.0 + 3000000.0);
+                return x;
+            }else if (id >= 62 && id < 3574){ // top 57% , unlimited trades
+                double x = 1;
+                while (x < 25000.0) x = Math.abs(r.nextGaussian() * 50000.0 + 8333.33);
                 return x;
             } else {
                 double x = 25000.0;
@@ -34,7 +39,7 @@ public class Population {
         }
 
         public String toString(){
-            return "ID: " + id + " PDT: "+ pdt + " net-worth: "+ netWorth;
+            return "ID: " + id + " PDT: "+ pdt + " initial net-worth: "+initNW+" final net-worth: "+ getNetworth() + "(" +getNetworth()/initNW *100.0+"%)"+" Number of trades: "+numberOfTrades +" Trades made: "+tradesmade.toString()+"\n";
         }
 
     }
