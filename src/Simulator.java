32bd4bb03151f2da738079c6a745ebeb66b6f217
jackmb <40642637+jackmb@users.noreply.github.com>
2020-05-16 17:49:15 -0400

    Added example dirs to lines that need changing

--- src/Simulator.java
+++ src/Simulator.java
@@ -3,17 +3,17 @@ import java.io.IOException;
 public class Simulator {
     public static void main(String[] args) throws InterruptedException, IOException {
 
-        StockExchange ibm = new StockExchange("LOCATION_OF_IBMCSV.csv", 4); //CHANGE THIS LINE
+        StockExchange ibm = new StockExchange("C:\\This\\Is\\An\\Example\\Directory\\.csv", 4); //CHANGE THIS LINE
 
         ibm.run("ibmout.txt");
         System.out.println("IBM Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\ibmout.txt");
 
-        StockExchange spy = new StockExchange("LOCATION_OF_SPYCSV.csv", 40); //CHANGE THIS LINE
+        StockExchange spy = new StockExchange("C:\\This\\Is\\An\\Example\\Directory\\SPYCSV.csv", 40); //CHANGE THIS LINE
 
         spy.run("spyout.txt");
         System.out.println("SPY Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\spyout.txt");
 
-        StockExchange vxx = new StockExchange("LOCATION_OF_VXXCSV.csv", 40); //CHANGE THIS LINE
+        StockExchange vxx = new StockExchange("C:\\This\\Is\\An\\Example\\Directory\\VXXCSV.csv", 40); //CHANGE THIS LINE
 
         vxx.run("vxxout.txt");
         System.out.println("VXX Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\vxxout.txt");
