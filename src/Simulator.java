57326fdde95a5def8d91321f6c875c7cbeb6ea8b
jackmb <40642637+jackmb@users.noreply.github.com>
2020-05-16 17:45:53 -0400

    Added markers to the .csv lines that need to be changed.

--- src/Simulator.java
+++ src/Simulator.java
@@ -2,13 +2,19 @@ import java.io.IOException;
 
 public class Simulator {
     public static void main(String[] args) throws InterruptedException, IOException {
-        StockExchange ibm = new StockExchange("C:\\Users\\Jack\\Desktop\\Desktop\\PSL\\GMU\\OR\\OR335\\IBMCSV2.csv", 4);
+
+        StockExchange ibm = new StockExchange("LOCATION_OF_IBMCSV.csv", 4); //CHANGE THIS LINE
+
         ibm.run("ibmout.txt");
         System.out.println("IBM Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\ibmout.txt");
-        StockExchange spy = new StockExchange("C:\\Users\\Jack\\Desktop\\Desktop\\PSL\\GMU\\OR\\OR335\\SPYCSV2.csv", 40);
+
+        StockExchange spy = new StockExchange("LOCATION_OF_SPYCSV.csv", 40); //CHANGE THIS LINE
+
         spy.run("spyout.txt");
         System.out.println("SPY Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\spyout.txt");
-        StockExchange vxx = new StockExchange("C:\\Users\\Jack\\Desktop\\Desktop\\PSL\\GMU\\OR\\OR335\\VXX_Trimmed.csv", 40);
+
+        StockExchange vxx = new StockExchange("LOCATION_OF_VXXCSV.csv", 40); //CHANGE THIS LINE
+
         vxx.run("vxxout.txt");
         System.out.println("VXX Market Day Simulated\n\tSee " + System.getenv("APPDATA") + "\\vxxout.txt");
     }
